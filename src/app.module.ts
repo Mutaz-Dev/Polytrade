ae7ba776218fe3dcf30b37502ed978d09e512d93
Mutaz-Dev <abualrub.mutaz@gmail.com>
2023-10-21 12:24:07 +0300

    build - postgres db config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,36 @@
-import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { Module, Logger } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService) => {
+        const logger = new Logger("InstanceLoader");
+        logger.verbose("connecting to DB")
+
+        return {
+          type: 'postgres',
+          database: config.get<string>('DB_NAME'),
+          username: config.get<string>('DB_USERNAME'),
+          password: config.get<string>('DB_PASSWORD'),
+          host: config.get<string>('DB_HOST'),
+          port: config.get<number>('DB_PORT'),
+          ssl: false,
+          connectTimeoutMS: config.get<number>('DB_TIMEOUT'),
+          entities: [],
+          //TODO: disable DB syncronization
+          synchronize: true,
+        };
+      }
+    })
+    ],
   controllers: [AppController],
   providers: [AppService],
 })
